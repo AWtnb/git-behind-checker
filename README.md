b64b89172588d40c01d384f46b0ca8c14ce73002
awtnb <arikaebanataw@gmail.com>
2025-06-16 23:00:35 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# README
+
+This Windows PowerShell script automatically checks each Git repository within a specified root directory and notifies you with a Windows toast notification if there are updates on the remote repository.
+
+
+## Modify if necessary
+
+```PowerShell
+$reposDir = $env:USERPROFILE | Join-Path -ChildPath "Personal\tools\repo" # Example
+```
+
+For instance, if your repositories are in C:\Users\YourUsername\Documents\GitHub, change it to:
+
+```PowerShell
+$reposDir = "C:\Users\YourUsername\Documents\GitHub".
+```
+
+## Notes
+
+- Git Installation: Git must be installed on the system running the script, and the git command should be accessible from the command prompt or PowerShell.
+- PowerShell Version: The toast notification function (`Invoke-Toast`) in this script might not work on PowerShell 6.0+ due to changes in how Universal Windows Platform (UWP) APIs are called. It should work without issues on PowerShell 5.1 (which is typically pre-installed on Windows).
---
+++ check-remote.ps1
@@ -0,0 +1,52 @@
+# Not works on PowerShell 6+.
+# https://ascii.jp/elem/000/004/059/4059715/
+
+function Invoke-Toast{
+    param (
+        [parameter(ValueFromPipeline = $true)][string]$message
+    )
+    $appId = "{1AC14E77-02E7-4E5D-B744-2EB1AE5198B7}\WindowsPowerShell\v1.0\powershell.exe"
+    $template = [Windows.UI.Notifications.ToastNotificationManager,Windows.UI.Notifications, ContentType = WindowsRuntime]::GetTemplateContent(
+        [Windows.UI.Notifications.ToastTemplateType, Windows.UI.Notifications, ContentType = WindowsRuntime]::ToastText01
+    )
+    $template.GetElementsByTagName("text").Item(0).InnerText=$message;
+    [Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($AppId).Show($template);
+}
+
+# Modify if necessary
+$reposDir = $env:USERPROFILE | Join-Path -ChildPath "Personal\tools\repo"
+
+if (-not (Test-Path $reposDir -PathType Container)) {
+    "ERROR:`nNot found: {0}" -f $reposDir | Invoke-Toast
+    exit 1
+}
+
+Get-ChildItem -Path $reposDir -Directory | ForEach-Object {
+    $repoPath = $_.FullName
+    $repoName = $_.Name
+
+    if (Test-Path (Join-Path $repoPath ".git") -PathType Container) {
+        Push-Location $repoPath
+        try {
+            git fetch --quiet
+            $localBranch = git rev-parse --abbrev-ref HEAD
+            $remoteTrackingBranch = git rev-parse --abbrev-ref --symbolic-full-name "@{u}" 2>$null
+            if ($null -eq $remoteTrackingBranch) {
+                "[git] {0}:`nERROR! Cannot find remote tracking branch corresponds to {1}" -f $repoName, $localBranch | Invoke-Toast
+            }
+            else {
+                $localCommit = git rev-parse $localBranch
+                $remoteCommit = git rev-parse $remoteTrackingBranch
+                if ($localCommit -ne $remoteCommit) {
+                    "[git] {0}:`nBehind to remote branch '{1}'" -f $repoName, $remoteTrackingBranch | Invoke-Toast
+                }
+            }
+        }
+        catch {
+            "[git] {0}:`nERROR! Something wrong happened during git controll" -f $repoName | Invoke-Toast
+        }
+        finally {
+            Pop-Location
+        }
+    }
+}
