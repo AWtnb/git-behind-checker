449d3d0c490d95835f8bc3b887c797d8399671f6
awtnb <arikaebanataw@gmail.com>
2025-06-20 09:56:26 +0900

    set dalay for startup task

--- register-task.ps1
+++ register-task.ps1
@@ -3,21 +3,29 @@ $action = New-ScheduledTaskAction -Execute conhost.exe -Argument "--headless pow
 $settings = New-ScheduledTaskSettingsSet -Hidden -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries
 
 $startupTaskName = "git-remote-check_startup"
+$startupTrigger = New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME
+$startupTrigger.Delay = [System.Xml.XmlConvert]::ToString((New-TimeSpan -Minutes 60))
+
 if ($null -ne (Get-ScheduledTask -TaskName $startupTaskName -ErrorAction SilentlyContinue)) {
     Unregister-ScheduledTask -TaskName $startupTaskName -Confirm:$false
 }
+
 Register-ScheduledTask -TaskName $startupTaskName `
     -Action $action `
-    -Trigger $(New-ScheduledTaskTrigger -AtLogOn -User $env:USERNAME) `
+    -Trigger $startupTrigger `
     -Description "Run git remote branch checker on startup." `
     -Settings $settings
 
+
 $dailyTaskName = "git-remote-check_daily"
+$dailyTrigger = New-ScheduledTaskTrigger -Daily -At "13:00"
+
 if ($null -ne (Get-ScheduledTask -TaskName $dailyTaskName -ErrorAction SilentlyContinue)) {
     Unregister-ScheduledTask -TaskName $dailyTaskName -Confirm:$false
 }
+
 Register-ScheduledTask -TaskName $dailyTaskName `
     -Action $action `
-    -Trigger $(New-ScheduledTaskTrigger -Daily -At "13:00") `
+    -Trigger $dailyTrigger `
     -Description "Run git remote branch checker on 13:00." `
     -Settings $settings
