92b0556ac808e8b72a039f2fcc0d99c538653b20
awtnb <arikaebanataw@gmail.com>
2025-11-23 22:14:12 +0900

    fix

--- check-remote.ps1
+++ check-remote.ps1
@@ -128,7 +128,7 @@ if ($behind.Count -gt 0) {
 else {
     "`nAll repos are up-to-date!" | Write-Host -ForegroundColor Black -BackgroundColor White
     15..1 | ForEach-Object {
-        "`r(close in {0:d2} seconds)" -f $_ | Write-Host -NoNewline
+        "`r(close in {0,2} seconds)" -f $_ | Write-Host -NoNewline
         Start-Sleep -Seconds 1
     }
 }
